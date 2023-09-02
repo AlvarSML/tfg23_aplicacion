f11f3000249bf95d8e296b509c45de2c4c917d47
AlvarSML <alvarsnow@gmail.com>
2023-09-02 22:25:27 +0200

    store

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import store from "./store";
+import { store } from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 
 loadFonts();
-
-createApp(App).use(store).use(vuetify).mount("#app");
+const app = createApp(App)
+app.use(store)
+app.use(store)
+app.use(vuetify)
+app.mount("#app");
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,21 +1,21 @@
 /* Creacion de la store */
+import { createStore } from 'vuex'
 
+/* legacy */
 import Vue from "vue";
-import Vuex, { StoreOptions } from "vuex";
 
 import { mainModule } from "./main";
 import { State } from "./state";
 import { adminModule } from "./admin";
 import { inferenceModule } from "./inference";
 
-Vue.use(Vuex);
 
-const storeOptions: StoreOptions<State> = {
+export const store = createStore ({
   modules: {
     main: mainModule,
     admin: adminModule,
     inference: inferenceModule
   }
-};
+})
 
-export default new Vuex.Store<State>(storeOptions);
+
